0f8b3d6e7afb8db2aab3e85de36a7ced7b8c984c
bahoocoding <bahoocoding@gmail.com>
2021-06-23 13:33:00 +0700

    create app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Header from "./components/Header.Component";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="grid-container">
+      <Header />
+      <main className="main">sdhdshdshsdhsd</main>
+      <footer className="footer">sshdshdshdhsdh</footer>
     </div>
   );
 }
---
+++ src/components/Header.Component.js
@@ -0,0 +1,10 @@
+
+
+const Header=(props)=>{
+
+    return <header className="header">
+      <div>header</div>
+
+    </header>
+}
+export default Header;
